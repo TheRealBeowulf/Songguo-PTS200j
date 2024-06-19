62bb2b6d6a6b571d0ae336d8e8107d75f58289a6
Ewan Parker <34030942+ewpa@users.noreply.github.com>
2024-06-19 22:44:41 +0100

    Always show tip temperature on standby screen, not 000 which is unsafe.

--- SolderingPen_ESP32S2/SolderingPen_ESP32S2.ino
+++ SolderingPen_ESP32S2/SolderingPen_ESP32S2.ino
@@ -551,9 +551,6 @@ void SENSORCheck() {
   if ((ShowTemp != Setpoint) || (abs(ShowTemp - CurrentTemp) > 5))
     ShowTemp = CurrentTemp;
   if (abs(ShowTemp - Setpoint) <= 1) ShowTemp = Setpoint;
-  if (inLockMode) {
-    ShowTemp = 0;
-  }
 
   // set state variable if temperature is in working range; beep if working
   // temperature was just reached
